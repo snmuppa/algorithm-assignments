cc2d617bc56373f93ce19c04a15afb5bfccdb1cd
Sai Muppa <snmuppa@users.noreply.github.com>
2020-05-26 15:26:45 -0700

    LeetCode: Update to the algo.

--- ArraysAndList/LengthOfLongestSubstring.java
+++ ArraysAndList/LengthOfLongestSubstring.java
@@ -1,29 +1,53 @@
+/**
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+Example 2:
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
 public class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
-        if(s == null || s.length() == 0) {
+        if(s == null || s.length() < 1 || s == "") {
             return 0;
+        } else if (s.length() == 1) {
+            return 1;
         }
         
-        int lenLongestSubStr = 0;
-        Set<Character> charSet = new HashSet<Character>();
+        int result = 1;
+        Set<Character> nonRepeatingSet = new HashSet<>();
+        nonRepeatingSet.add(s.charAt(0));
+        
         int lagPointer = 0;
-        for(int currPointer = 0; currPointer < s.length(); currPointer ++) {
-            char charAtCurrPointer = s.charAt(currPointer);
-            if(charSet.contains(charAtCurrPointer)) {
+        
+        for(int currPointer = 1; currPointer < s.length(); currPointer++) {
+            if(!nonRepeatingSet.contains(s.charAt(currPointer))) {
+                nonRepeatingSet.add(s.charAt(currPointer));
+                result = Math.max(result, nonRepeatingSet.size());
+            }  else {
                 while(lagPointer < currPointer) {
-                    char charAtLagPointer = s.charAt(lagPointer);
-                    lagPointer ++;
-                    charSet.remove(charAtLagPointer);
-                    System.out.println(charSet);
-                    if(charAtCurrPointer == charAtLagPointer) {
-                        break;
-                    } 
+                   if(s.charAt(lagPointer) == s.charAt(currPointer)) {
+                       lagPointer++;
+                       break;
+                   }
+                   nonRepeatingSet.remove(s.charAt(lagPointer));
+                   lagPointer++;
                 }
             }
-            charSet.add(charAtCurrPointer);
-            lenLongestSubStr = Math.max(charSet.size(), lenLongestSubStr);
         }
         
-        return lenLongestSubStr;
+        return result;
     }
 }
